bcf8f00094b5b25ad79bf09591f0be5a0062f8a8
Jorgepvnv <jorgepvnv@gmail.com>
2022-09-26 10:11:57 +0200

    Upload App

--- Project/src/main/java/Test/Project/App.java
+++ Project/src/main/java/Test/Project/App.java
@@ -1,7 +1,12 @@
 package Test.Project;
+import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hola mundo! esto es un código de prueba para el examen");
+	Scanner in = new Scanner(System.in);
+	
+    System.out.println("Buenos días. ¿Cómo te llamas?:  ");
+    String s = in.nextLine();
+    System.out.println("Te quiero, " + s);
   }
 }
